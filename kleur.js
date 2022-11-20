09f12808ed717dabde0f6aecb869d09e16dc5245
jaapvangils <96062312+jaapvangils@users.noreply.github.com>
2022-11-20 14:57:36 +0100

    aangepast in 2 files

---
+++ kleur.js
@@ -0,0 +1,19 @@
+var telkliks = 0;
+
+function getRandomColor() {
+  var letters = '0123456789ABCDEF';
+  var color = '#';
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
+
+function myFunction() {
+  const myCollection = document.getElementsByTagName("p");
+  for (let i = 0; i < myCollection.length; i++) {
+    myCollection[i].style.color = getRandomColor();
+  }
+  telkliks++;
+  document.getElementById("teller").innerHTML="Aantal keer geklikt:" + telkliks;
+}
