6f6307f32d123b64ddf943de467decd8494e258e
jaapvangils <96062312+jaapvangils@users.noreply.github.com>
2022-11-20 15:46:06 +0100

    comment toegevoegd

--- kleur.js
+++ kleur.js
@@ -1,6 +1,8 @@
+//initieren variabele buiten de functies.
 var telkliks = 0;
-alert("start");
 
+// functie om een random kleur te fabriceren op basis van 
+// hexadecimale code (#000000-#FFFFFF)
 function getRandomColor() {
   var letters = '0123456789ABCDEF';
   var color = '#';
@@ -10,11 +12,16 @@ function getRandomColor() {
   return color;
 }
 
-function myFunction() {
+// functie om alle kleuren van de elementen 'p' aan te passen
+function kleuraanpassen() {
+  // alle p regels komen in de const (array)
   const myCollection = document.getElementsByTagName("p");
+  // met een for lus van 0 tot het aantal "p"'s in de tekst
   for (let i = 0; i < myCollection.length; i++) {
     myCollection[i].style.color = getRandomColor();
   }
+  // optellen van het aantal kliks/hovers
   telkliks++;
+  // weergeven van de uitkomst van de teller op de html pagina, gekoppeld aan een ID
   document.getElementById("teller").innerHTML="Aantal keer geklikt:" + telkliks;
 }
