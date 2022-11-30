2e68eae525da1056078eeb673049aded790f871e
Jaap van Gils <0104106@zadkine.nl>
2022-11-30 10:07:43 +0100

    aanpassingen les

--- canvas/canvas1.js
+++ canvas/canvas1.js
@@ -1,7 +1,16 @@
-var canvas = document.getElementById("myCanvas");
-var ctx = canvas.getContext("2d");
-var ctx2 = canvas.getContext("2d");
-ctx.font = "20px Arial";
-ctx.fillText("Hallo!", 70,70);
-ctx2.fillStyle = "#00FF00";
-ctx2.fillRect(10,10,180,20);
+var canvas = document.getElementById("mijnCanvas"); // variabele canvas gevuld met verwijzing 
+var ctx = canvas.getContext("2d"); // ctx gevuld met toevoeging 2d
+var vierhoek = canvas.getContext("2d"); // vierhoek ook naar de 2d omgeving verwijzen
+
+// tekst schrijven in canvas
+ctx.font = "20px Arial"; // toekennen font aan object ctx
+ctx.fillText("Hallo",80,180); // 
+
+vierhoek.fillStyle = "#DD00DD";
+vierhoek.fillRect(10,10, 180, 40);
+
+
+
+// alternatief voor regel 1-6
+//document.getElementById("mijnCanvas").getContext("2d").font = "20px Arial";
+//document.getElementById("mijnCanvas").getContext("2d").fillText("tekst",10,10);
--- canvas/canvas2.js
+++ canvas/canvas2.js
@@ -1,20 +1,22 @@
-var mijnObject; // in dit object plaatsen we een Rectangle
+var mijnObject; // aan dit object koppelen we een figuur
+var canvasNieuw;
 
-// opstarten Canvas vanuit JavaScript
+//Canvas creëeren
 function startCanvas() {
-	canvasNw = document.createElement("canvas"); // een element canvas aan de array canvas toevoegen.
-    canvasNw.width = 480;
-    canvasNw.height = 360;
-    document.body.insertBefore(canvasNw, document.body.childNodes[0]);
-    mijnObject = new onsObject(30, 30, "red", 20, 120);
+    canvasNieuw = document.createElement("canvas"); // canvas definiëren
+    canvasNieuw.width = 480;
+    canvasNieuw.height = 360;
+    document.body.insertBefore(canvasNieuw, document.body.childNodes[0]);
+    mijnObject = new objectMaker(30,30,"red", 20, 120);
 }
 
-function onsObject(width, height, color, x, y) {
-    this.width = width;
-    this.height = height;
+// object maken
+function objectMaker(breedte, hoogte, kleur, x, y) {
+    this.breedte = breedte;
+    this.hoogte = hoogte;
     this.x = x;
-    this.y = y;    
-    ctx = canvasNw.getContext("2d");
-    ctx.fillStyle = color;
-    ctx.fillRect(this.x, this.y, this.width, this.height);
+    this.y = y;
+    ctx = canvasNieuw.getContext("2d");
+    ctx.fillStyle = kleur;
+    ctx.fillRect(this.x,this.y, this.breedte, this.hoogte);
 }
