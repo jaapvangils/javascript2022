0685b31a8dcf6bb5d586fff22bc5b36d1de673cf
Jaap van Gils <0104106@zadkine.nl>
2022-12-09 14:26:23 +0100

    na lessen aanpassingen

--- canvas/blokje.js
+++ canvas/blokje.js
@@ -1,46 +1,71 @@
 var MyCanvas = document.getElementById("MyCanvas"); // koppeling naar Canvas
-var ctx = MyCanvas.getContext("2d"); // ctx => 2d canvas
+var ctx = MyCanvas.getContext("2d"); // koppelen aan 2d modus
 
-// variabelen voor beweging
-var SpritePositionX = 50; // plek horizontaal
-var SpritePositionY = 50; // plek verticaal
-var speed = 5; // snelheid van bewegen sprite
+var SpritePositieX = Math.floor(Math.random()*480); // positie X var
+var SpritePositieY = Math.floor(Math.random()*480); // positie Y var
+var stapGrootte = 5; // sneldheid van bewegen blokje
 
 tekenSprite();
 
-document.addEventListener("keydown", (event) => {
-    let keypressed = event.key; // uitlezen event / toetsenbord
-    console.log(keypressed); // uitlezen in log (F12)
-    switch (keypressed) { // we testen de inhoud van keypressed
-        case "ArrowUp": // als de inhoud van keypressed omhoog is
-            SpritePositionY -= speed; // Y = Y - speed
-            tekenSprite(); // tekenen van het scherm
-            break;
-        case "ArrowDown": 
-            SpritePositionY += speed;
+document.addEventListener("keydown", (uitlezen) => {
+    let toets = uitlezen.key; // uitlezen van 1 toetsindruk
+    switch (toets) {
+        case "ArrowUp": // pijl omhoog => Y verminderen met stapgrootte
+            SpritePositieY -= stapGrootte;
             tekenSprite();
             break;
-        case "ArrowLeft":
-            SpritePositionX -= speed;
+        case "ArrowDown":
+            SpritePositieY += stapGrootte;
             tekenSprite();
             break;
         case "ArrowRight":
-            SpritePositionX += speed;
+            SpritePositieX += stapGrootte;
             tekenSprite();
             break;
-        default: break;
+        case "ArrowLeft":
+            SpritePositieX -= stapGrootte;
+            tekenSprite();
+            break;
+        case " ": // bij spatie -> springen
+            springOmhoog(); // functie spring omhoog
+            break;
+            
+        default: break; // standaard (dus bij andere indruk): direct afbreken
     }
 });
 
+
+// functie om een vertraging in te bouwen
+function sleep(ms) {
+    return new Promise(
+      resolve => setTimeout(resolve, ms)
+    );
+  }
+
+async function springOmhoog() { // asynchrone functie: deze neemt wat tijd in beslag.
+    for (i = 0; i < 15; i++) // loop om 15x omhoog te bewegen
+    {
+        SpritePositieY -= 3;
+        await sleep(25); // met await gaat het script pas verder nadat sleep (25) klaar is met afspelen.
+        tekenSprite(); // elke keer een afdruk maken
+    }
+    for (i = 0; i < 15; i++) // en weer terugbewegen.
+    {
+        SpritePositieY += 3;
+        await sleep(25);
+        tekenSprite();
+    }
+}
+
 function tekenSprite() {
-    let breed = MyCanvas.offsetWidth-20;
-    let hoog = MyCanvas.offsetHeight-20;
-    if (SpritePositionX < 0) { SpritePositionX = 0;}
-    if (SpritePositionY < 0) { SpritePositionY = 0;}
-    if (SpritePositionX > breed) {SpritePositionX = breed;}
-    if (SpritePositionY > hoog) {SpritePositionY = hoog;}
-    ctx.clearRect(0,0, MyCanvas.offsetWidth, MyCanvas.offsetHeight);
-    ctx.fillStyle = "red";
-    ctx.fillRect(SpritePositionX, SpritePositionY, 20, 20);
+    if (SpritePositieX < 0) { SpritePositieX = MyCanvas.offsetWidth - 20; }
+    if (SpritePositieY < 0) { SpritePositieY = MyCanvas.offsetHeight - 20; }
+    if (SpritePositieX > MyCanvas.offsetWidth - 20) 
+        { SpritePositieX = 0; }
+    if (SpritePositieY > MyCanvas.offsetHeight - 20 ) 
+        { SpritePositieY = 0; }
+    ctx.clearRect(0, 0, MyCanvas.offsetWidth, MyCanvas.offsetHeight); // leegmaken
+    ctx.fillStyle = "green"; // kleur toekennen
+    ctx.fillRect(SpritePositieX, SpritePositieY, 20, 20); // vierkant tekenen
 }
 
---
+++ canvas/sprite.js
@@ -0,0 +1,30 @@
+let img = new Image();
+img.src = 'https://opengameart.org/sites/default/files/Green-Cap-Character-16x18.png';
+img.onload = function() {
+  window.requestAnimationFrame(gameLoop);
+};
+
+let canvas = document.querySelector('canvas');
+let ctx = canvas.getContext('2d');
+
+const SCALE = 2;
+const WIDTH = 16;
+const HEIGHT = 18;
+const SCALED_WIDTH = SCALE * WIDTH;
+const SCALED_HEIGHT = SCALE * HEIGHT;
+
+function drawFrame(frameX, frameY, canvasX, canvasY) {
+  ctx.drawImage(img,
+                frameX * WIDTH, frameY * HEIGHT, WIDTH, HEIGHT,
+                canvasX, canvasY, SCALED_WIDTH, SCALED_HEIGHT);
+}
+
+const CYCLE_LOOP = [0, 1, 0, 2];
+let currentLoopIndex = 0;
+let frameCount = 0;
+let currentDirection = 0;
+
+function gameLoop() {
+
+  window.requestAnimationFrame(gameLoop);
+}
