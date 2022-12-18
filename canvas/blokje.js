820c05c972b745c0ba4dd1cef27af17209aa4d36
jaapvangils <96062312+jaapvangils@users.noreply.github.com>
2022-12-18 22:08:28 +0100

    aanpassingen in scripts vb les

--- canvas/blokje.js
+++ canvas/blokje.js
@@ -1,70 +1,124 @@
 var MyCanvas = document.getElementById("MyCanvas"); // koppeling naar Canvas
 var ctx = MyCanvas.getContext("2d"); // koppelen aan 2d modus
 
-var SpritePositieX = Math.floor(Math.random()*480); // positie X var
-var SpritePositieY = Math.floor(Math.random()*480); // positie Y var
-var stapGrootte = 5; // sneldheid van bewegen blokje
+var SpritePositieX = SpriteFormaat; // positie X var
+var SpritePositieY = SpriteFormaat; // positie Y var
+var SpriteFormaat = 50;
+var stapGrootte = 1; // sneldheid van bewegen blokje
+var sprongSpr2 = 20;
+var tijdVoorWissel = 250;
+var richtingSpr2x = 1;
+var richtingSpr2y = 1;
+var Spr2formaat = 40;
+var ingedrukteToets = [];
+var crash = 0;
+setInterval(speelVeldUpdate, 5);
+var Spr2x = MyCanvas.offsetWidth - Spr2formaat;
+var Spr2y = MyCanvas.offsetHeight - Spr2formaat;
 
-tekenSprite();
-
+//luisteren naar toetsenbord in EventListener (soort interupt)
 document.addEventListener("keydown", function (uitlezen) {
-    let toets = (uitlezen.keys || []); // uitlezen van 1 toetsindruk
-    
-    if (toets[38]) {
-         // pijl omhoog => Y verminderen met stapgrootte
-            SpritePositieY -= stapGrootte;
-            tekenSprite();
-    }
-    if (toets[40]) { 
-            SpritePositieY += stapGrootte;
-            tekenSprite();
-    }
-    if (toets[39]) {
-            SpritePositieX += stapGrootte;
-            tekenSprite();
-    }
-    if (toets[37]) {
-            SpritePositieX -= stapGrootte;
-            tekenSprite();
-        }
-    if (toets[32]) { // bij spatie -> springen
-            springOmhoog(); // functie spring omhoog
-    }
-            
+  ingedrukteToets = ingedrukteToets || [];
+  ingedrukteToets[uitlezen.keyCode] = true;
 });
 
+document.addEventListener("keyup", function (e) {
+  ingedrukteToets[e.keyCode] = false;
+});
 
-// functie om een vertraging in te bouwen
-function sleep(ms) {
-    return new Promise(
-      resolve => setTimeout(resolve, ms)
-    );
+function speelVeldUpdate() {
+  if (ingedrukteToets && ingedrukteToets[38]) {
+    SpritePositieY -= stapGrootte;
   }
-
-async function springOmhoog() { // asynchrone functie: deze neemt wat tijd in beslag.
-    for (i = 0; i < 20; i++) // loop om 15x omhoog te bewegen
-    {
-        SpritePositieY -= 2;
-        await sleep(25); // met await gaat het script pas verder nadat sleep (25) klaar is met afspelen.
-        tekenSprite(); // elke keer een afdruk maken
-    }
-    for (i = 0; i < 20; i++) // en weer terugbewegen.
-    {
-        SpritePositieY += 2;
-        await sleep(25);
-        tekenSprite();
-    }
+  if (ingedrukteToets && ingedrukteToets[40]) {
+    SpritePositieY += stapGrootte;
+  }
+  if (ingedrukteToets && ingedrukteToets[39]) {
+    SpritePositieX += stapGrootte;
+  }
+  if (ingedrukteToets && ingedrukteToets[37]) {
+    SpritePositieX -= stapGrootte;
+  }
+  //if (ingedrukteToets && ingedrukteToets[32]) { springOmhoog(); // functie spring omhoog }
+  tekenSprite();
+}
+function checkRanden() {
+  if (SpritePositieX < 0) {
+    SpritePositieX = MyCanvas.offsetWidth - SpriteFormaat;
+  }
+  if (SpritePositieY < 0) {
+    SpritePositieY = MyCanvas.offsetHeight - SpriteFormaat;
+  }
+  if (SpritePositieX > MyCanvas.offsetWidth - SpriteFormaat) {
+    SpritePositieX = 0;
+  }
+  if (SpritePositieY > MyCanvas.offsetHeight - SpriteFormaat) {
+    SpritePositieY = 0;
+  }
 }
 
 function tekenSprite() {
-    if (SpritePositieX < 0) { SpritePositieX = MyCanvas.offsetWidth - 20; }
-    if (SpritePositieY < 0) { SpritePositieY = MyCanvas.offsetHeight - 20; }
-    if (SpritePositieX > MyCanvas.offsetWidth - 20) 
-        { SpritePositieX = 0; }
-    if (SpritePositieY > MyCanvas.offsetHeight - 20 ) 
-        { SpritePositieY = 0; }
-    ctx.clearRect(0, 0, MyCanvas.offsetWidth, MyCanvas.offsetHeight); // leegmaken
-    ctx.fillStyle = "green"; // kleur toekennen
-    ctx.fillRect(SpritePositieX, SpritePositieY, 20, 20); // vierkant tekenen
+  ctx.clearRect(0, 0, MyCanvas.offsetWidth, MyCanvas.offsetHeight); // leegmaken
+  ctx.fillStyle = "green"; // kleur toekennen
+  ctx.fillRect(SpritePositieX, SpritePositieY, SpriteFormaat, SpriteFormaat); // vierkant tekenen
+
+  Spr2();
+  botsing();
+  checkRanden();
+  ctx.fillStyle = "red"; // kleur toekennen
+  ctx.fillRect(Spr2x, Spr2y, Spr2formaat, Spr2formaat); // vierkant tekenen
 }
 
+function Spr2() {
+  sprongSpr2++;
+  if (sprongSpr2 > tijdVoorWissel) {
+    sprongSpr2 = 0;
+    richtingSpr2x = Math.floor(Math.random() * 3) - 1;
+    richtingSpr2y = Math.floor(Math.random() * 3) - 1;
+  }
+  Spr2x += richtingSpr2x;
+  Spr2y += richtingSpr2y;
+  // check randen
+  if (Spr2x < 0) {
+    Spr2x = MyCanvas.offsetWidth - Spr2formaat;
+  }
+  if (Spr2y < 0) {
+    Spr2y = MyCanvas.offsetHeight - Spr2formaat;
+  }
+  if (Spr2x > MyCanvas.offsetWidth - Spr2formaat) {
+    Spr2x = 0;
+  }
+  if (Spr2y > MyCanvas.offsetHeight - Spr2formaat) {
+    Spr2y = 0;
+  }
+}
+
+function botsing() {
+  // test voor botsing.
+  var helftHoogteSprite = SpriteFormaat / 2;
+  var Spr1boven = SpritePositieY - helftHoogteSprite;
+  var Spr1beneden = SpritePositieY + helftHoogteSprite;
+  var Spr1links = SpritePositieX - helftHoogteSprite;
+  var Spr1rechts = SpritePositieX + helftHoogteSprite;
+  var helftGrooteSpr2 = Spr2formaat / 2;
+  var Spr2boven = Spr2y - helftGrooteSpr2;
+  var Spr2beneden = Spr2y + helftGrooteSpr2;
+  var Spr2links = Spr2x - helftGrooteSpr2;
+  var Spr2rechts = Spr2x + helftGrooteSpr2;
+  if (
+    Spr1beneden < Spr2boven ||
+    Spr1boven > Spr2beneden ||
+    Spr1rechts < Spr2links ||
+    Spr1links > Spr2rechts
+  ) {
+    // niets
+  } else {
+    crash++;
+    Spr2x = 100;
+    Spr2y = 100;
+    SpritePositieX = MyCanvas.offsetWidth - 100;
+    SpritePositieY = MyCanvas.offsetHeight - 100;
+    document.getElementById("crash").innerHTML = "Crashes: " + crash;
+    sprongSpr2 = tijdVoorWissel;
+  }
+}
