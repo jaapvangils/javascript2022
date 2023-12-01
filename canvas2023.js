b2a77790357cb187dfc23b8819c1034d65f39e81
jaapvangils <96062312+jaapvangils@users.noreply.github.com>
2023-12-01 13:27:26 +0100

    aangepast

--- canvas2023.js
+++ canvas2023.js
@@ -1,12 +1,10 @@
 //we maken een canvas met 800x600 en voegen toe aan de html pagina
-
 let canvas = document.createElement('canvas');
 let ctx = canvas.getContext('2d');
 canvas.width = 800;
 canvas.height = 600;
-ctx.fillStyle = '#FF0000';
 document.body.appendChild(canvas);
 
-ctx.beginPath();
-ctx.rect(20, 20, 150, 100);
-ctx.stroke();
+ctx.beginPath(); // start met tekenen
+ctx.rect(20, 20, 150, 100); // teken een vierhoek
+ctx.stroke(); //schrijf de tekening weg
