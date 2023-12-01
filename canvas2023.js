eff0223c796cdbee5530938842876b4f979dc94e
jaapvangils <96062312+jaapvangils@users.noreply.github.com>
2023-12-01 10:21:19 +0100

    Update canvas2023.js

--- canvas2023.js
+++ canvas2023.js
@@ -7,43 +7,6 @@ canvas.height = 600;
 ctx.fillStyle = '#FF0000';
 document.body.appendChild(canvas);
 
-// Variabelen voor de positie en snelheid van de cirkel
-let x = 50; // startpositie X
-let y = 50; // startpositie Y
-let dx = 2; // snelheid X
-let dy = 2; // snelheid Y
-let radius = 20; // radius van de cirkel
-
-// functie om een circel te tekenen
-function drawCircle() {
-    ctx.beginPath();
-    ctx.arc(x, y, radius, 0, Math.PI * 2, true);
-    ctx.fillStyle = 'blue';
-    ctx.fill();
-    ctx.closePath();
-}
-
-function update() {
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.fillStyle = "#FEFEFE";
-    ctx.fillRect(0, 0, canvas.width, canvas.height);
-
-    drawCircle();
-
-    // Verander de positie van de cirkel
-    x += dx; // =  x=x+dx;
-    y += dy;
-
-    // Laat de cirkel stuiteren wanneer hij de randen van het canvas raakt
-    if (x + dx > canvas.width - radius || x + dx < radius) {
-        dx = -dx;
-    }
-    if (y + dy > canvas.height - radius || y + dy < radius) {
-        dy = -dy;
-    }
-
-    requestAnimationFrame(update); // Roep update weer aan voor de volgende frame
-}
-
-// Start de animatieloop
-update();
+ctx.beginPath();
+ctx.rect(20, 20, 150, 100);
+ctx.stroke();
