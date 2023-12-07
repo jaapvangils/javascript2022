6210f38b5f5a34b134e9b6c2efc68f4453bf6ad2
jaapvangils <96062312+jaapvangils@users.noreply.github.com>
2023-12-07 23:52:55 +0100

    les2

--- canvas2023_2e_bal.js
+++ canvas2023_2e_bal.js
@@ -5,12 +5,19 @@ canvas.width = 800;
 canvas.height = 600;
 document.body.appendChild(canvas);
 
+
 // Variabelen voor de positie en snelheid van de cirkel
 let x = 50; // startpositie X
 let y = 50; // startpositie Y
-let dx = 2; // snelheid X
-let dy = 2; // snelheid Y
+let dx = 0; // snelheid X , 0 om nog even niet te starten
+let dy = 0; // snelheid Y
 let radius = 20; // radius van de cirkel
+
+// Variabelen voor de tweede bal
+let x2 = 100; // startpositie X voor de tweede bal
+let y2 = 100; // startpositie Y voor de tweede bal
+let radius2 = 20; // radius van de tweede bal
+let speed = 5; // snelheid waarmee de tweede bal beweegt
 
 // De functie om de cirkel te tekenen
 function drawCircle() {
@@ -21,6 +28,37 @@ function drawCircle() {
     ctx.closePath();
 }
 
+// Functie om de tweede bal te tekenen
+function drawSecondCircle() {
+    ctx.beginPath();
+    ctx.arc(x2, y2, radius2, 0, Math.PI * 2, true);
+    ctx.fillStyle = 'red';
+    ctx.fill();
+    ctx.closePath();
+}
+
+// Event listener voor toetsaanslagen
+document.addEventListener('keydown', function(event) {
+    if (event.key === 'ArrowLeft') {
+        x2 -= speed;
+    } else if (event.key === 'ArrowRight') {
+        x2 += speed;
+    } else if (event.key === 'ArrowUp') {
+        y2 -= speed;
+    } else if (event.key === 'ArrowDown') {
+        y2 += speed;
+    }
+});
+
+function startMoving() {
+    dx = 2;
+    dy = 2;
+}
+
+// Voeg een event listener toe aan de knop
+document.getElementById('startButton').addEventListener('click', startMoving);
+
+
 // De update functie die elke frame wordt uitgevoerd
 function update() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
@@ -28,6 +66,8 @@ function update() {
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
     drawCircle();
+
+    drawSecondCircle(); // Teken de tweede bal
 
     // Verander de positie van de cirkel
     x += dx;
